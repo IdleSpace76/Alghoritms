70ddff97c23d47fe6f8b3e44c2e65b646a5f56b5
Andrei Zharov <a.zharov@yarsec.ru>
2023-09-11 12:36:48 +0300

    Binary search

---
+++ src/main/java/ru/alghoritms/search/BinarySearch.java
@@ -0,0 +1,43 @@
+package ru.alghoritms.search;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * Бинарный поиск в упорядоченном массиве
+ *
+ * @author a.zharov
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] list = IntStream.rangeClosed(1, 100).toArray();
+
+        Scanner scanner = new Scanner(System.in);
+        int item;
+
+        do {
+            System.out.println("Think the num from 1 to 100: ");
+            item = scanner.nextInt();
+        }
+        while (item < 1 || item > 100);
+
+        int min = 0, max = list.length - 1, iterCount = 0, mid, guess;
+
+        while (true) {
+            iterCount++;
+            mid = (max + min) / 2;
+            guess = list[mid];
+            if (guess == item) {
+                break;
+            }
+            else if (guess > item) {
+                max = mid - 1;
+            }
+            else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println("Num: " + guess + "\nIterations: " + iterCount);
+    }
+}
