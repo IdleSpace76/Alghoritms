a7ab9bc5f7af5baee6341737af784fef4910ff51
Andrei Zharov <a.zharov@yarsec.ru>
2023-09-20 14:53:40 +0300

    QuickSort

--- src/main/java/ru/alghoritms/sort/QuickSort.java
+++ src/main/java/ru/alghoritms/sort/QuickSort.java
@@ -14,14 +14,43 @@ public class QuickSort {
             arr[i] = (int)(Math.random() * 100);
             System.out.print(" " + arr[i]);
         }
+        System.out.println();
 
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.print("New arr:");
+        for (int i : arr) {
+            System.out.print(" " + i);
+        }
     }
 
-    public static int[] sort(int[] arr) {
-        if (arr.length < 2) {
-            return arr;
+    private static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
         }
 
-        return newArr;
+        int swapTemp = arr[i + 1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
     }
 }
